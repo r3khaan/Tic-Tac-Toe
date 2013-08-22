e60d2220dd6d7467937fc400a33cfa9f595c5a42
Rehaan <rehaanirf@yahoo.com>
2013-08-22 14:24:47 -0700

    Added 3 images; modified RunMain
    Images were x, o, and board

--- TicTacToe/src/RunMain.java
+++ TicTacToe/src/RunMain.java
@@ -1,11 +1,12 @@
+import javax.swing.JFrame;
 
-public class RunMain {
+public class RunMain extends JFrame{
 	
 	public RunMain() {
 		
 	}
 	
 	public static void main(String args[]) {
-		
+		new RunMain();
 	}
 }
